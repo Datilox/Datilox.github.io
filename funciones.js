4e534585ac2e73be9cd8331e02a0eebe5285d1c2
Datilox <shoutmoon1112@gmail.com>
2023-03-04 19:08:39 -0300

    Se agrega el ProyectoCV

---
+++ funciones.js
@@ -0,0 +1,26 @@
+
+function Redireccion(){
+    location.href = 'https://www.instagram.com/'
+}
+
+function Redireccion1(){
+    location.href = 'https://twitter.com/?lang=es'
+}
+
+function Redireccion2(){
+    location.href = 'https://es-la.facebook.com/'
+}
+
+function Redireccion3(){
+    location.href = 'https://ar.linkedin.com/'
+}
+
+function Redireccion4(){
+    location.href = 'https://discord.com/'
+}
+
+
+document.getElementById('button').addEventListener('click', function(){
+    document.getElementById('button').innerHTML= "GRACIAS!"  
+});
+
